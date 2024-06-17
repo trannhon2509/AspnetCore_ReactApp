677b8c3504989ce222dd42ab5fb31e1e6f0646af
nhonv <nhonv@DESKTOP-P7L14C1>
2024-06-17 16:18:01 +0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using AspnetCore_ReactApp.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Reflection.Emit;
+
+namespace AspnetCore_ReactApp.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            if (entityType.GetTableName().StartsWith("AspNet"))
+            {
+                entityType.SetTableName(entityType.GetTableName().Substring(6));
+            }
+        }
+       
+
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspnetCore_ReactApp.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
